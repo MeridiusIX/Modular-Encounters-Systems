190a0f3fbd4a0f2980dfb20cceeeec3566d2f023
James Stephenson <82529529+Synirrr@users.noreply.github.com>
2024-02-27 15:53:14 -0700

    Fixed bad NaN comparison with floating point numbers producing Arithmetic Exception

--- Data/Scripts/ModularEncountersSystems/Behavior/Subsystems/AutoPilot/RotationSystem.cs
+++ Data/Scripts/ModularEncountersSystems/Behavior/Subsystems/AutoPilot/RotationSystem.cs
@@ -271,7 +271,7 @@ namespace ModularEncountersSystems.Behavior.Subsystems.AutoPilot {
 
 		public float LimitSpeed(float proposedSpeed, double actualSpeed) {
 
-			if (proposedSpeed == float.NaN || actualSpeed == double.NaN)
+			if (float.IsNaN(proposedSpeed) || double.IsNaN(actualSpeed))
 				return 0;
 
 			if (proposedSpeed == 0 || Math.Sign(proposedSpeed) != Math.Sign(actualSpeed))

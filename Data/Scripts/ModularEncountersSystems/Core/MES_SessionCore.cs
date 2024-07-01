f59f6387a00c6fc9ed0843dfef487985dd569b2c
CptArthur <59366211+CptArthur@users.noreply.github.com>
2024-07-01 18:19:47 +0200

    2.71.13

--- Data/Scripts/ModularEncountersSystems/Core/MES_SessionCore.cs
+++ Data/Scripts/ModularEncountersSystems/Core/MES_SessionCore.cs
@@ -36,8 +36,8 @@ namespace ModularEncountersSystems.Core {
 		public static bool SyncWarning = false;
 		public bool FinalSetup = false;
 
-		public static string ModVersion = "2.71.12";
-		public static int ModVersionValue = 200710012; //Use above value as reference - 3 digits per part (
+		public static string ModVersion = "2.71.13";
+		public static int ModVersionValue = 200710013; //Use above value as reference - 3 digits per part (
 		public static MES_SessionCore Instance;
 
 		public static bool IsServer;
